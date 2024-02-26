495df1ef3788c4767532d28e12e659b23bd68285
rinrab <tima@chemodax.net>
2024-02-26 18:22:42 +0100

    Add logging of notification service stopping

--- ESchoolBot/NotificationService.cs
+++ ESchoolBot/NotificationService.cs
@@ -52,6 +52,8 @@ namespace ESchoolBot
 
                 await Task.Delay(TimeSpan.FromSeconds(fetchDelay), stoppingToken);
             }
+
+            logger.LogInformation($"Notification service stopped");
         }
 
         private async Task FetchUser(DatabaseClient.User user, CancellationToken stoppingToken)
