3536169837f83ab13ae764eb95b6d668ddbdde2b
rinrab <tima@chemodax.net>
2024-02-26 21:51:03 +0100

    Cleanup

--- ESchoolBot.Tests/Tests.cs
+++ ESchoolBot.Tests/Tests.cs
@@ -14,34 +14,5 @@ namespace ESchoolBot.Tests
                 await client.LoginAsync("<login>", "<password>", default);
             }
         }
-
-        [Test]
-        public void DatabaseTests()
-        {
-            //IDatabaseAccessor databaseAccessor = new DatabaseAccessor("Data Source=test.db");
-
-            //// Create table and upgrade
-            //IDatabaseClient databaseClient = new DatabaseClient(databaseAccessor);
-
-            //// Add user
-            //databaseClient.InsertUser(new DatabaseClient.User
-            //{
-            //    ChatId = 123,
-            //    Username = "amogus_username",
-            //    Password = "amogus_password",
-            //    SessionId = "amogus_session",
-            //    ProcessedDate = new DateTime(2020, 1, 4),
-            //    IsEnabled = true
-            //});
-
-            //// Get users
-            //var list = databaseClient.ListUsers();
-            //Assert.AreEqual(123, list[0].ChatId);
-            //Assert.AreEqual(true, list[0].IsEnabled);
-
-            //databaseClient.DisableUser(123);
-
-            //Assert.AreEqual(false, databaseClient.ListUsers()[0].IsEnabled);
-        }
     }
 }
